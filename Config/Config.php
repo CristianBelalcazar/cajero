022f425e2198eb214ee2ab2e9ad268adbaacd43f
Cristian Belalcazar <CristianBelalcazar9511@gmail.com>
2024-06-06 14:45:52 -0500

    version 1

---
+++ Config/Config.php
@@ -0,0 +1,8 @@
+<?php 
+const base_url = "http://localhost/cajero/";
+const host = "localhost";
+const user = "root";
+const password = "";
+const db = "sistema";
+const charset = "charset=utf8";
+?>
---
+++ Models/UsuariosModel.php
@@ -0,0 +1,24 @@
+<?php
+ class UsuariosModel extends Query{
+    public function __construct()
+    {
+          parent::__construct();
+    }
+    public function getUsuario(string $usuario, string $clave)
+    {
+        $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND clave = '$clave'";
+        $data = $this->select($sql);
+        return $data;
+
+    }    
+    public function getUsuarios()
+    {
+        $sql = "SELECT * FROM usuarios";
+        $data = $this->selectAll($sql);
+        return $data;
+
+    }    
+ }
+
+
+?>
